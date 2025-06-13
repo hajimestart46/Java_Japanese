b2997837f2576862d46dbe58f1ba1c65bf1ceb01
hajimestart46 <xinyuxia96@gmail.com>
2025-06-13 11:43:14 +0900

    Object study

--- Study/src/Study_Object/BasicObject.java
+++ Study/src/Study_Object/BasicObject.java
@@ -22,10 +22,15 @@ public class BasicObject {
 			＊戻り値：メソッドの処理の後に呼び出し元に返す値、returnで指定する。
 			何も返さない場合は、戻り値の型にvoidを記述する
 		 */
-		Student stu1 = new Student();
-		stu1.name = "山田 太郎";
+		
+		//Studentクラスはインスタンス化
+		Student stu1 = new Student("山田 太郎");
+		Student stu2 = new Student("佐藤 太郎");
+		//呼び出すメソッドは名前と引数の組合せで決めている
 		stu1.setScore(60, 60);
 		stu1.display();
+		//オブジェクトを生成しなくても利用できる static型
+		Student.info();
 	}
 }
 
@@ -34,18 +39,42 @@ class Student {
 	String name;
 	int engScore;
 	int mathScore;
+	int phyScore;
+	//学生をカウンタする
+	static int counter = 0;
 	
+	
+	//コンストラクタ
+	/*	
+ 	＊名前がクラス名と同じ
+ 	＊戻り値を持たない
+ 	＊new クラス名(コンストラクタへの引数)
+	*/
+	Student(String n) {
+		name = n;
+		System.out.println(n + "さんをインスタンス化しました");
+		counter++;
+	}
 	//メソッド
 	void display() {
 		System.out.println(name + "さん");
-		System.out.println("英語" + engScore + "点・数学" + mathScore + "点");
+		System.out.println("英語" + engScore + "点・数学" + mathScore + "点・物理" + phyScore + "点");
 	}
 	void setScore(int eng, int math) {
 		engScore = eng;
 		mathScore = math;
 	}
+	//オーバーロード
+	void setScore(int eng, int math, int phy) {
+		engScore = eng;
+		mathScore = math;
+		phyScore = phy;
+	}
 	double getAvg() {
 		double avg = (engScore + mathScore) / 2.0;
 		return avg;
+	}
+	static void info() {
+		System.out.println(counter + "名学生です");
 	}
 }
